44969289c93ddde9269bfc5687c0b8494c3ff6db
yahya <yahyamamoudou.sow.etu@univ-lille.fr>
2024-07-17 22:06:41 +0200

    removing association

--- association/src/accueil.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Accueil from './accueil';
-
-test('renders learn react link', () => {
-  render(<Accueil />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
